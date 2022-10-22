bfc7f8c05caec673144908c41c126de20444f373
StefanTodoran <stefan.alex4@gmail.com>
2022-10-22 16:09:07 -0700

    added basic visualizer, very scuffed

--- build/tsc.js
+++ build/tsc.js
@@ -3,13 +3,15 @@
     window.addEventListener('load', init);
     function init() {
         var testList = [
-            { name: "Carrots", group: "Vegetable", fridge: 21, pantry: 7 },
-            { name: "Broccoli", group: "Vegetable", fridge: 5, pantry: 2 },
-            { name: "Milk", group: "Dairy", fridge: 7 },
-            { name: "Bread", group: "Grains", pantry: 4, fridge: 14 },
-            { name: "Pancake Mix", group: "Grains", pantry: 12 },
+            { name: "Carrots", group: "vegetable", fridge: 21, pantry: 7 },
+            { name: "Minced Beef", group: "meat", fridge: 2, freezer: 5 },
+            { name: "Broccoli", group: "vegetable", fridge: 5, pantry: 2 },
+            { name: "Milk", group: "dairy", fridge: 7 },
+            { name: "Bread", group: "grains", pantry: 4, fridge: 14 },
             { name: "Jam", on_open: 365 },
+            { name: "Pancake Mix", group: "grains", pantry: 12 },
             { name: "Margarine", on_open: 90 },
+            { name: "Apples", group: "fruit", pantry: 12, fridge: 24 },
         ];
         var scan_btn = document.getElementById('scan-btn');
         scan_btn.addEventListener('click', function () {
@@ -19,10 +21,32 @@
     }
     function displayItems(items) {
         var container = document.getElementById('visualizer');
+        var template = document.getElementById('template');
         container.classList.add('active');
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
-            console.log(item);
+            var node = template.cloneNode(true);
+            node.classList.remove('hidden');
+            node.querySelector('h2').textContent = item.name;
+            if (item.group) {
+                node.classList.add(item.group);
+            }
+            addDuration(node, "pantry", item.pantry);
+            addDuration(node, "fridge", item.fridge);
+            addDuration(node, "freezer", item.freezer);
+            addDuration(node, "on_open", item.on_open);
+            container.insertBefore(node, template.nextSibling);
+        }
+    }
+    function addDuration(node, type, duration) {
+        console.log(node, type, duration);
+        var indicator = node.querySelector("." + type);
+        if (duration) {
+            indicator.style.setProperty('--days', duration.toString());
+            indicator.style.setProperty('--time', duration.toString() + " days");
+        }
+        else {
+            indicator.classList.add('hidden');
         }
     }
 })();
--- src/index.ts
+++ src/index.ts
@@ -32,13 +32,15 @@
     // dynamic_elements.forEach((element) => observer.observe(element));
 
     const testList:FoodItem[] = [
-      {name: "Carrots", group: "Vegetable", fridge: 21, pantry: 7},
-      {name: "Broccoli", group: "Vegetable", fridge: 5, pantry: 2},
-      {name: "Milk", group: "Dairy", fridge: 7},
-      {name: "Bread", group: "Grains", pantry: 4, fridge: 14},
-      {name: "Pancake Mix", group: "Grains", pantry: 12},
+      {name: "Carrots", group: "vegetable", fridge: 21, pantry: 7},
+      {name: "Minced Beef", group: "meat", fridge: 2, freezer: 5},
+      {name: "Broccoli", group: "vegetable", fridge: 5, pantry: 2},
+      {name: "Milk", group: "dairy", fridge: 7},
+      {name: "Bread", group: "grains", pantry: 4, fridge: 14},
       {name: "Jam", on_open: 365},
+      {name: "Pancake Mix", group: "grains", pantry: 12},
       {name: "Margarine", on_open: 90},
+      {name: "Apples", group: "fruit", pantry: 12, fridge: 24},
     ];
 
     const scan_btn = document.getElementById('scan-btn');
@@ -63,11 +65,38 @@
 
   function displayItems(items: FoodItem[]) {
     const container = document.getElementById('visualizer');
+    const template = document.getElementById('template');
     container.classList.add('active');
 
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
-      console.log(item);
+      // @ts-ignore
+      const node:HTMLElement = template.cloneNode(true);
+
+      node.classList.remove('hidden');
+      node.querySelector('h2').textContent = item.name;
+      if (item.group) {
+        node.classList.add(item.group);
+      }
+
+      addDuration(node, "pantry", item.pantry);
+      addDuration(node, "fridge", item.fridge);
+      addDuration(node, "freezer", item.freezer);
+      addDuration(node, "on_open", item.on_open);
+
+      // insert after template
+      container.insertBefore(node, template.nextSibling);
+    }
+  }
+
+  function addDuration(node: HTMLElement, type: string, duration: number) {
+    console.log(node, type, duration);
+    const indicator:HTMLElement = node.querySelector("." + type);
+    if (duration) {
+      indicator.style.setProperty('--days', duration.toString());
+      indicator.style.setProperty('--time', duration.toString() + " days");
+    } else {
+      indicator.classList.add('hidden');
     }
   }
 })();
