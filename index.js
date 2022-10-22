8b5c0151b62b0683de595abf2f056e28f131c950
StefanTodoran <stefan.alex4@gmail.com>
2022-10-22 11:18:28 -0700

    added site template & icons

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+(function () {
+
+  window.addEventListener('load', init);
+
+  /**
+   * Initialization function that should handle anything that needs to occur
+   * on page load (include changing from one page to another).
+   */
+  function init() {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.4
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('shown');
+        }
+      })
+    }, options);
+    
+    const dynamic_elements = document.querySelectorAll('.dynamic-item');
+    dynamic_elements.forEach((element) => observer.observe(element));
+  }
+})();
