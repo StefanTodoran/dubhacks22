05db6a121a3d4a5602e5af8275bd2bf5d8b7bae5
StefanTodoran <stefan.alex4@gmail.com>
2022-10-22 14:07:12 -0700

    setting up food item vis

--- build/tsc.js
+++ build/tsc.js
@@ -2,21 +2,22 @@
 (function () {
     window.addEventListener('load', init);
     function init() {
-        var options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.4
-        };
-        var observer = new IntersectionObserver(function (entries) {
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('shown');
-                }
-            });
-        }, options);
-        var dynamic_elements = document.querySelectorAll('.dynamic-item');
-        dynamic_elements.forEach(function (element) { return observer.observe(element); });
-        console.log("test");
+        var testList = [
+            { name: "Carrots", group: "Vegetable", fridge: 21, pantry: 7 },
+            { name: "Broccoli", group: "Vegetable", fridge: 5, pantry: 2 },
+            { name: "Milk", group: "Dairy", fridge: 7 },
+            { name: "Bread", group: "Grains", pantry: 4, fridge: 14 },
+            { name: "Pancake Mix", group: "Grains", pantry: 12 },
+            { name: "Jam", on_open: 365 },
+            { name: "Margarine", on_open: 90 },
+        ];
+        displayItems(testList);
+    }
+    function displayItems(items) {
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            console.log(item);
+        }
     }
 })();
 //# sourceMappingURL=tsc.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,65 @@
    * on page load (include changing from one page to another).
    */
   function init() {
-    interface Options {
-      root: any;
-      rootMargin: string;
-      threshold: number;
+    // interface Options {
+    //   root: any;
+    //   rootMargin: string;
+    //   threshold: number;
+    // }
+
+    // let options:Options = {
+    //   root: null,
+    //   rootMargin: '0px',
+    //   threshold: 0.4
+    // };
+
+    // const observer = new IntersectionObserver((entries) => {
+    //   entries.forEach((entry) => {
+    //     if (entry.isIntersecting) {
+    //       entry.target.classList.add('shown');
+    //     }
+    //   })
+    // }, options);
+
+    // const dynamic_elements = document.querySelectorAll('.dynamic-item');
+    // dynamic_elements.forEach((element) => observer.observe(element));
+
+    // const testList:FoodItem[] = [
+    //   {name: "Carrots", group: "Vegetable", days: 21, fridge: true, tip: ""},
+    //   {name: "Broccoli", group: "Vegetable", days: 5, fridge: true, tip: ""},
+    //   {name: "Milk", group: "Dairy", days: 7, fridge: true}
+    // ];
+
+    const testList:FoodItem[] = [
+      {name: "Carrots", group: "Vegetable", fridge: 21, pantry: 7},
+      {name: "Broccoli", group: "Vegetable", fridge: 5, pantry: 2},
+      {name: "Milk", group: "Dairy", fridge: 7},
+      {name: "Bread", group: "Grains", pantry: 4, fridge: 14},
+      {name: "Pancake Mix", group: "Grains", pantry: 12},
+      {name: "Jam", on_open: 365},
+      {name: "Margarine", on_open: 90},
+    ];
+
+    displayItems(testList);
+  }
+
+  interface FoodItem {
+    name: string,
+    
+    // until expiration in days, provide at least 1
+    pantry?: number,
+    fridge?: number,
+    freezer?: number,
+    on_open?: number,
+    
+    group?: string, // food type 
+    tip?: string,
+  }
+
+  function displayItems(items: FoodItem[]) {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      console.log(item);
     }
-
-    let options:Options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.4
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('shown');
-        }
-      })
-    }, options);
-
-    const dynamic_elements = document.querySelectorAll('.dynamic-item');
-    dynamic_elements.forEach((element) => observer.observe(element));
-
-
-    // input:
-    // food item -> mapped to expiration date
-
-    // output:
-    // visualize that data
-
-    console.log("test");
   }
 })();
