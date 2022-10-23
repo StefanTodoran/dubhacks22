5bf3bb78dc058eb8e30c3d9e3a5f95819ab862ee
njbradley <nicholasjohnstonbradley@gmail.com>
2022-10-22 19:10:10 -0700

    added jimp

--- src/receipt.js
+++ src/receipt.js
@@ -7,7 +7,7 @@ async function parseReceipt(img_element, logger) {
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
     await worker.setParameters({
-        tessedit_pageseg_mode: Tesseract.PSM.SINGLE_COLUMN,
+        //tessedit_pageseg_mode: Tesseract.PSM.SINGLE_COLUMN,
         tessedit_char_whitelist: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ",
     });
     const data = await worker.recognize(img_element);
